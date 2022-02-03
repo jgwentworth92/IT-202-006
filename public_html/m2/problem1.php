50e3e190eb4f169e68de5e9f3e5452a80234fa99
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2022-02-03 13:09:17 -0600

    Add files via upload

--- public_html/m2/problem1.php
+++ public_html/m2/problem1.php
@@ -12,12 +12,17 @@ function processArray($arr) {
 
       foreach ($arr as $value) {
         if($value%2!=0){
+<<<<<<< HEAD
+            echo "$value <br>";}
+          } 
+=======
             echo "odd numbers: <br>".$value;}
           
+>>>>>>> 9ef2fe3f9b1cb23ba70608530711e5e0b4b254d9
     
-      }
+      
+        }
 
-}
 echo "Problem 1: Odd Output<br>";
 ?>
 <table>
