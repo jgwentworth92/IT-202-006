1cbeeae87211b4023b1343d23a016191c25dfada
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-02-28 14:11:13 -0600

    complete

--- lib/function.php
+++ lib/function.php
@@ -4,9 +4,37 @@ require_once(__dir__."/db.php");
 
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
-//TODO 2: filter helpers
+require(__DIR__ . "/sanitizers.php");
 
 //TODO 3: User helpers
+require(__DIR__ . "/user_helpers.php");
 
 //TODO 4: Flash Message Helpers
+function flash($msg =" ",$color="info")
+{
+    $message=["text"=> $msg, "color"=> $color];
+if (isset($_SESSION['flash']))
+{
+array_push($_SESSION['flash'],$message);
+}
+else{ 
+    $_SESSION['flash']=array();
+    array_push($_SESSION['flash'],$message);
+
+}
+
+}
+
+function getMessages()
+{
+    if (isset($_SESSION['flash']))
+    {
+        $flashes=$_SESSION['flash'];
+        $_SESSION['flash']=array();
+        return $flashes;
+    }
+    return array();
+
+}
+
 ?>
--- public_html/project/login.php
+++ public_html/project/login.php
@@ -10,11 +10,7 @@ require(__DIR__ . "/../../partials/nav.php");
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
-    <div>
-        <label for="confirm">Confirm</label>
-        <input type="password" name="confirm" required minlength="8" />
-    </div>
-    <input type="submit" value="Register" />
+    <input type="submit" value="Login" />
 </form>
 <script>
     function validate(form) {
@@ -26,15 +22,10 @@ require(__DIR__ . "/../../partials/nav.php");
 </script>
 <?php
 //TODO 2: add PHP Code
-if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
+if (isset($_POST["email"]) && isset($_POST["password"])) {
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
-    $confirm = se(
-        $_POST,
-        "confirm",
-        "",
-        false
-    );
+
     //TODO 3
     $hasError = false;
     if (empty($email)) {
@@ -52,30 +43,33 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         flash("password must not be empty");
         $hasError = true;
     }
-    if (empty($confirm)) {
-        flash("Confirm password must not be empty");
-        $hasError = true;
-    }
     if (strlen($password) < 8) {
         flash("Password too short");
         $hasError = true;
     }
-    if (
-        strlen($password) > 0 && $password !== $confirm
-    ) {
-        flash("Passwords must match");
-        $hasError = true;
-    }
     if (!$hasError) {
         //TODO 4
-        $hash = password_hash($password, PASSWORD_BCRYPT);
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+        $stmt = $db->prepare("SELECT email, password from Users where email = :email");
         try {
-            $stmt->execute([":email" => $email, ":password" => $hash]);
-            flash("Successfully registered!");
+            $r = $stmt->execute([":email" => $email]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
+                    unset($user["password"]);
+                    if (password_verify($password, $hash)) {
+                        flash("Weclome $email");
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
+                    } else {
+                        flash("Invalid password");
+                    }
+                } else {
+                    flash("Email not found");
+                }
+            }
         } catch (Exception $e) {
-            flash("There was a problem registering");
             flash("<pre>" . var_export($e, true) . "</pre>");
         }
     }
