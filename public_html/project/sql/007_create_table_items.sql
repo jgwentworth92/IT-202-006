36cf313c3c7697fa0587b5a28da285ce67e7340a
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 13:32:15 -0500

    07 table fixed

--- public_html/project/sql/007_create_table_items.sql
+++ public_html/project/sql/007_create_table_items.sql
@@ -6,10 +6,11 @@ CREATE TABLE IF NOT EXISTS Products(
     stock int DEFAULT  0,
     unit_price int DEFAULT  99999, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
     image text, -- this col type can't have a default value; this isn't required for any project, I chose to add it for mine
+    is_visible         TINYINT(1)  default 1,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     check (stock >= 0), -- don't allow negative stock; I don't allow backorders
-    check (unit price >= 0) , -- don't allow negative costs
-    is_visible         TINYINT(1)  default 1
+    check (unit_price >= 0) , -- don't allow negative costs
+    
 )
 
