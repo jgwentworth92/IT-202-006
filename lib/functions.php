df6cf1d10e4958e2b980e505e36ffe84159c113e
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 12:39:14 -0500

    first push feature items milestone 2

--- lib/functions.php
+++ lib/functions.php
@@ -22,4 +22,8 @@ require(__DIR__ . "/duplicate_user_details.php");
 //reset session
 require(__DIR__ . "/reset_session.php");
 require(__DIR__ . "/get_url.php");
+require(__DIR__ . "/get_columns.php");
+
+require(__DIR__ . "/input_map.php");
+
 ?>
