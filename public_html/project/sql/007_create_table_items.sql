56f638e48a8ad1833863e5620a69fe6943276c1c
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 17:30:25 -0500

    view and add

--- public_html/project/sql/007_create_table_items.sql
+++ public_html/project/sql/007_create_table_items.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS Products(
     id int AUTO_INCREMENT PRIMARY  KEY,
     name varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
     description text,
+    category varchar(30),
     stock int DEFAULT  0,
     unit_price DECIMAL(19 , 4 ) NOT NULL, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
     image text, -- this col type can't have a default value; this isn't required for any project, I chose to add it for mine
