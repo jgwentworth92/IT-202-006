2d92390b83f995a63341dde377d83bcbdb2a8762
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 14:38:58 -0500

    e

--- public_html/project/sql/007_create_table_items.sql
+++ public_html/project/sql/007_create_table_items.sql
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS `Products`(
-    `id` int AUTO_INCREMENT PRIMARY  KEY,
-    `SKU` varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
-    `description` text,
-    `category` VARCHAR(30),
-    `stock` int DEFAULT  0,
-    `unit_price` int DEFAULT  99999, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
-    image text, -- this col type can't have a default value; this isn't required for any project, I chose to add it for mine
-    `is_visible`  TINYINT(1)  default 1,
-    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-=======
+
 CREATE TABLE IF NOT EXISTS Products(
     id int AUTO_INCREMENT PRIMARY  KEY,
     SKU varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
@@ -22,9 +10,7 @@ CREATE TABLE IF NOT EXISTS Products(
     is_visible TINYINT(1)  default 1,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
->>>>>>> 8b4902bd470587897be6b80e3fef9a64a94c9154
     check (stock >= 0), -- don't allow negative stock; I don't allow backorders
-    check (unit_price >= 0)  -- don't allow negative costs
-    
+    check (unit_price >= 0)  -- don't allow negative costs   
 )
 
