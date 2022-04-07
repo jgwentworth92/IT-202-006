cf34676396b8bee020b463a6cfd59fa9dea4ffa6
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 16:35:38 -0500

    fixed db

--- public_html/project/sql/007_create_table_items.sql
+++ public_html/project/sql/007_create_table_items.sql
@@ -1,18 +1,16 @@
 
 CREATE TABLE IF NOT EXISTS Products(
-    id int NOT NULL AUTO_INCREMENT P,
-    SKU varchar(30) NOT NULL , -- alternatively you'd have a SKU that's unique
+    id int AUTO_INCREMENT PRIMARY  KEY,
+    name varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
     description text,
-    category VARCHAR(30),
     stock int DEFAULT  0,
-    unit_price int DEFAULT  99999, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
+    unit_price DECIMAL(19 , 4 ) NOT NULL, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
     image text, -- this col type can't have a default value; this isn't required for any project, I chose to add it for mine
-    is_visible TINYINT(1)  default 1,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-     PRIMARY KEY (`id`),
-    UNIQUE(`SKU`),
+    is_visible TINYINT(1) default 1
+    ,
     check (stock >= 0), -- don't allow negative stock; I don't allow backorders
-    check (unit_price >= 0)  -- don't allow negative costs   
+    check (cost >= 0) -- don't allow negative costs
 )
 
