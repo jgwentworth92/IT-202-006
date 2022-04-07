d646329f12be1dc1a4cf61d590193bdaf8656f3c
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 12:42:50 -0500

    Update 007_create_table_items.sql
    boolean update

--- public_html/project/sql/007_create_table_items.sql
+++ public_html/project/sql/007_create_table_items.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS Products(
     stock int DEFAULT  0,
     unit_price int DEFAULT  99999, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
     image text, -- this col type can't have a default value; this isn't required for any project, I chose to add it for mine
-    is_visible         TINYINT(1)  default 1,
+    is_visible boolean DEFAULT false,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     check (stock >= 0), -- don't allow negative stock; I don't allow backorders
