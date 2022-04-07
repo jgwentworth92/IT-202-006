339ead0c18953c685f0a6a7705107f65999470c0
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-07 15:17:46 -0500

    moved save_Data to lib folder

--- lib/functions.php
+++ lib/functions.php
@@ -24,6 +24,8 @@ require(__DIR__ . "/reset_session.php");
 require(__DIR__ . "/get_url.php");
 require(__DIR__ . "/get_columns.php");
 
+require(__DIR__ . "/save_data.php");
+
 require(__DIR__ . "/input_map.php");
 
 ?>
