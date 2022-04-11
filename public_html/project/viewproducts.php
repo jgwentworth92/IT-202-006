47429d16f3ffaba5d1b02be700eff25c16a2ff65
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-10 23:06:44 -0500

    milestone 2 order by filters

--- public_html/project/viewproducts.php
+++ public_html/project/viewproducts.php
@@ -20,59 +20,57 @@ try {
     flash("Error fetching records category information", "danger");
 }
 $cat = se($_GET, "category", "", false);
-$name=se($_GET,"itemName","",false);
+$name = se($_GET, "itemName", "", false);
 $base_query = "SELECT id, name, description, stock, unit_price, image FROM $TABLE_NAME ";
-if(isset($_GET["highest"]))
-{$col=" unit_price ";
-$order= " ASC ";
+if (isset($_GET["highest"])) {
+    $col = " unit_price ";
+    $order = " ASC ";
 }
 
-if(isset($_GET["lowest"]))
-{$col=" unit_price ";
-$order= " DESC ";
+if (isset($_GET["lowest"])) {
+    $col = " unit_price ";
+    $order = " DESC ";
 }
 if (!empty($col) && !empty($order)) {
     $query .= " ORDER BY $col $order"; //be sure you trust these values, I validate via the in_array checks above
 }
 
 $query = " WHERE 1=1"; //1=1 shortcut to conditionally build AND clauses
-$query .=" AND is_visible =1";
+$query .= " AND is_visible =1";
 $params = [];
 if (!empty($cat)) {
     $query .= " AND  category = :category";
     $params[":category"] = "$cat";
 }
-if(!empty($name))
-{
+if (!empty($name)) {
     $query .= " AND  name like :name";
     $params[":name"] = "%$name%";
-
 }
 
 
 
 
-    $query .= " LIMIT 50";
-    error_log(var_export($params, true));
-    error_log(var_export($name, true));
-    error_log(var_export($cat, true));
-    error_log(var_export($query, true));
-    $stmt = $db->prepare($base_query . $query);
-    foreach ($params as $key => $value) {
-        $type = is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
-        $stmt->bindValue($key, $value, $type);
+$query .= " LIMIT 50";
+error_log(var_export($params, true));
+error_log(var_export($name, true));
+error_log(var_export($cat, true));
+error_log(var_export($query, true));
+$stmt = $db->prepare($base_query . $query);
+foreach ($params as $key => $value) {
+    $type = is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
+    $stmt->bindValue($key, $value, $type);
+}
+$params = null;
+try {
+    $stmt->execute($params);
+    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($r) {
+        $results = $r;
     }
-    $params = null;
-    try {
-        $stmt->execute($params);
-        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        if ($r) {
-            $results = $r;
-        }
-    } catch (PDOException $e) {
-        error_log(var_export($e, true));
-        flash("Error fetching records we in it bby", "danger");
-    }
+} catch (PDOException $e) {
+    error_log(var_export($e, true));
+    flash("Error fetching records we in it bby", "danger");
+}
 
 
 
@@ -86,24 +84,27 @@ if(!empty($name))
     <form method="GET" class="row row-cols-lg-auto g-3 align-items-center">
         <div class="input-group  mr-2 mb-3">
             <input class="form-control" type="search" name="itemName" placeholder="Item Filter" />
-      
+
 
             <select method="GET" class="form-select" aria-label="Default select example">
-            <option value="0">--Select Category--</option>
+                <option value="0">--Select Category--</option>
                 <?php foreach ($category_list as $dropdown) : ?>
-                    
-                    <option value="<?php se($dropdown, "category");    error_log(var_export($dropdown, true)); ?>" name="category">
+
+                    <option value="<?php se($dropdown, "category");
+                                    error_log(var_export($dropdown, true)); ?>" name="category">
                         <?php se($dropdown, "category");    ?>
                     </option>
                 <?php endforeach;  ?>
             </select>
-
-            <select class="form-select" aria-label="Default select example">
-  <option selected>Price Filter</option>
-  <option name="highest" value="highest"> Highest to lowest</option>
-  <option name="lowest "value="lowest">Lowest to highest</option>
-  
-</select>
+            <div class="custom-control custom-radio">
+                <input type="radio" class="custom-control-input" id="defaultUnchecked" name="lowest">
+                <label class="custom-control-label" for="defaultUnchecked">low to high</label>
+            </div>
+            <div class="custom-control custom-radio">
+                <input type="radio" class="custom-control-input" id="defaultUnchecked" name="highest">
+                <label class="custom-control-label" for="defaultUnchecked">high to low</label>
+            </div>
+            \
             <input class="btn btn-primary" type="submit" value="Search" />
 
     </form>
