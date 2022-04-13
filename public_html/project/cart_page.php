6fb4fed46924dab7e3a2854a9bab05de09518c43
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-13 03:07:06 -0500

    first draft cart page

--- public_html/project/cart_page.php
+++ public_html/project/cart_page.php
@@ -7,6 +7,8 @@ require(__DIR__ . "/../../partials/nav.php");
 
 $TABLE_NAME = "Products";
 $Cart_Table="JG_Cart";
+$id = se($_GET, "id", -1, false);
+
 if (isset($_POST["stock"]))  {
     $amount= se($_POST, "stock", "", false);
     
@@ -26,28 +28,6 @@ if (isset($_POST["submit"])) {
 }
 
 
-$result = [];
-$columns = get_columns($TABLE_NAME);
-//echo "<pre>" . var_export($columns, true) . "</pre>";
-$ignore = ["id", "modified", "created"];
-$db = getDB();
-//get the item
-$id = se($_GET, "id", -1, false);
-$stmt = $db->prepare("SELECT * FROM $TABLE_NAME where id =:id");
-try {
-    $stmt->execute([":id" => $id]);
-    $r = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($r) {
-        $result = $r;
-    }
-} catch (PDOException $e) {
-    error_log(var_export($e, true));
-    flash("Error looking up record", "danger");
-}
-
-$columns = get_columns($TABLE_NAME);
-//echo "<pre>" . var_export($columns, true) . "</pre>";
-$ignore = ["id", "modified", "created"];
 ?>
 
 
@@ -56,37 +36,11 @@ $ignore = ["id", "modified", "created"];
 
 
 <div class="container-fluid">
-    <h1>Shop</h1>
-    <div class="row">
-        <div class="col">
-            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
-                <?php foreach ($results as $item) : ?>
-                    <div class="col">
-                        <div class="card bg-light" style="height:25em">
-                            <div class="card-header">
-                                JG shop.
-                            </div>
-                            <div class="card-body">
-                            <h5 class="card-title">Name: <?php se($item, "name"); ?></h5>
-                                <p class="card-text">Description: <?php se($item, "description"); ?></p>
-                                <p class="card-text">Category: <?php se($item, "category"); ?></p>
-                                <p class="card-text">Stock: <?php se($item, "stock"); ?></p>
-                            </div>
-                            <div class="card-footer">
-                                Cost: <?php se($item, "unit_price"); ?>
-                              
-                            </div>
-                        </div>
-                        
-                    </div>
-                <?php endforeach; ?>
-                <div class="mb-3">
-                <form method="POST">
+         <form method="POST">
             <label class="form-label" for="stock">Quantity</label>
             <input class="form-control"  type="number" step="1"" name="confirm" required  />
-            </form>
-             </div>
-                <input class="btn btn-primary" type="submit" value="Create" name="submit" />
-            </div>
-        </div>
-        <div class="col-4" style="min-width:30em">
+         </form>
+            
+         <input class="btn btn-primary" type="submit" value="Create" name="submit" />
+          
+<div class="col-4" style="min-width:30em">
