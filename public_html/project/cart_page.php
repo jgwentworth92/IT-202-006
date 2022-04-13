4f2922d564a6dbe354198b34521ca9456aa238b9
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-13 17:05:27 -0500

    cart add

--- public_html/project/cart_page.php
+++ public_html/project/cart_page.php
@@ -8,10 +8,12 @@ require(__DIR__ . "/../../partials/nav.php");
 $TABLE_NAME = "Products";
 $Cart_Table="JG_Cart";
 $id = se($_GET, "id", -1, false);
-
+error_log(var_export($id, true));
 $userID= get_username();
+error_log(var_export($userID, true));
 if (isset($_POST["submit"])) {
     $amount= se($_POST, "stock", "", false);
+    error_log(var_export($amount, true));
         $db = getDB();
         $stmt = $db->prepare("INSERT INTO $Cart_Table (item_id, quantity, user_id) VALUES(:item, :quantity, :userID)");
         try {
