445f9c25e4da57be65ad7e486dfcf6d58b30368e
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-15 04:12:55 -0500

    modal test

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -118,6 +118,25 @@ if (isset($_POST["delete"])) {
                 </div>
             </div>
             <div class="col-4" style="min-width:30em">
+            <div class="modal fade" id="my-modal" role="dialog" aria-labelledby="my-modal-label" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<a class="btn" data-toggle="modal" href="#my-modal">Launch Modal</a>
             <?php endif; ?>
 
             <?php
