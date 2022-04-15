0ccc4e00f3e1aef079dc9d72f58eecab0f949232
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-15 10:00:23 -0500

    cart page refactor

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -7,6 +7,21 @@ $user_id = get_user_id();
 $results = [];
 $db = getDB();
 
+if (isset($_POST["delete"])) {
+    $db = getDB();
+    $line_id = (int)se($_POST, "lineID", null, false);
+
+    $stmt = $db->prepare("DELETE FROM JG_Cart where id = :id and :uid");
+    try {
+        //added user_id to ensure the user can only delete their own items
+        $stmt->execute([":id" => $line_id, ":uid" => $user_id]);
+
+        http_response_code(200);
+    } catch (PDOException $e) {
+        error_log("Error deleting line item: " . var_export($e, true));
+        flash("error removing", "warning");
+    }
+}
 
 $stmt = $db->prepare("SELECT name, c.id as line_id, item_id, quantity, unit_price, (unit_price*quantity) as subtotal FROM JG_Cart c JOIN Products i on c.item_id = i.id WHERE c.user_id = :uid");
 try {
@@ -24,21 +39,6 @@ try {
     flash("Error fetching records", "danger");
 }
 
-if (isset($_POST["delete"])) {
-    $db = getDB();
-    $line_id = (int)se($_POST, "lineID", null, false);
-
-    $stmt = $db->prepare("DELETE FROM JG_Cart where id = :id and :uid");
-    try {
-        //added user_id to ensure the user can only delete their own items
-        $stmt->execute([":id" => $line_id, ":uid" => $user_id]);
-
-        http_response_code(200);
-    } catch (PDOException $e) {
-        error_log("Error deleting line item: " . var_export($e, true));
-        flash("error removing", "warning");
-    }
-}
 
 
 
@@ -51,7 +51,6 @@ if (isset($_POST["delete"])) {
         <h1> Total: $ <?php se($total_cost, null, "N/A"); ?></h1>
         <div class="row">
             <div class="col">
-                
                 <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
                     <?php foreach ($results as $item) : ?>
                         <div class="col">
@@ -64,52 +63,35 @@ if (isset($_POST["delete"])) {
                                     <p class="card-text">price: <?php se($item, "unit_price"); ?></p>
                                     <p class="card-text">Amount: <?php se($item, "quantity"); ?></p>
                                     <p class="card-text">Subtotal: <?php se($item, "subtotal"); ?></p>
-
+                                    <form method="POST">
+                                        <input class="form-control" type="hidden" name="lineID" value="<?php se($item, "line_id"); ?>" />
+                                        <input class="btn btn-primary" type="submit" value="Delete" name="delete" />
+                                    </form>
                                 </div>
                                 <div class="card-footer">
+                                    Cost: <?php se($item, "unit_price"); ?>
+
                                     <?php if (is_logged_in()) : ?>
-                                        <button type="button" id="pro_popup" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"> work my guy </button>
-                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog" role="document">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <form>
-                                                            <div class="form-group">
-                                                                <form action="add_to_cart.php" method="POST">
-                                                                    <label class="form-label" for="amount">Quantity</label>
-                                                                    <input class="form-control" type="number" step="1" name="amount" required />
-                                                                    <input class="form-control" type="hidden" name="item_id" value="<?php se($item, "item_id"); ?>" />
-                                                                    <input class="btn btn-primary" type="submit" value="Update" name="submit" />
-                                                                </form>
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <form method="POST">
-                                                                    <input class="form-control" type="hidden" name="lineID" value="<?php se($item, "line_id"); ?>" />
-                                                                    <input class="btn btn-primary" type="submit" value="Delete" name="delete" />
-                                                                </form>
-                                                            </div>
-                                                        </form>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                        <button type="button" class="btn btn-primary">Send message</button>
-                                                    </div>
-                                                </div>
+                                        <form action="add_to_cart.php" class="form-inline" method="POST">
+                                            <div class="form-group mb-2">
+                                                <label class="form-label" for="amount">Quantity</label>
+                                                <input class="form-control" type="number" step="1" name="amount" required />
                                             </div>
-                                        </div>
+                                            <input class="form-control" type="hidden" name="item_id" value="<?php se($item, "item_id"); ?>" />
+                                            <input class="btn btn-primary" type="submit" value="Update" name="submit" />
+                                        </form>
+                                        <form method="POST">
+                                            <input class="form-control" type="hidden" name="lineID" value="<?php se($item, "line_id"); ?>" />
+                                            <input class="btn btn-primary" type="submit" value="Delete" name="delete" />
+                                        </form>
+
                                     <?php endif; ?>
                                 </div>
                             </div>
                             <?php if (has_role("Admin")) : ?>
 
                                 <td>
-                                    <a href="<?php echo get_url('admin/edit_item.php'); ?>?id=<?php se($item, "item_id"); ?>">Edit</a>
+                                    <a href="<?php echo get_url('admin/edit_item.php'); ?>?id=<?php se($item, "id"); ?>">Edit</a>
                                 </td>
                             <?php endif; ?>
                         </div>
@@ -118,25 +100,6 @@ if (isset($_POST["delete"])) {
                 </div>
             </div>
             <div class="col-4" style="min-width:30em">
-            <div class="modal fade" id="my-modal" role="dialog" aria-labelledby="my-modal-label" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        <p>One fine body&hellip;</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<a class="btn" data-toggle="modal" href="#my-modal">Launch Modal</a>
             <?php endif; ?>
 
             <?php
@@ -159,9 +122,5 @@ if (isset($_POST["delete"])) {
                         isValid = false;
                     }
                     return isValid;
-
-    
-
                 }
-       
             </script>
