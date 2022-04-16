b0a472b81e4160c8e6f3e0284c64456bcbe18829
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-16 00:06:57 -0500

    image upload

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -114,7 +114,7 @@ try {
         </h1>
         <div class="row">
             <div class="col">
-                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
+                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4 g-4">
                     <?php foreach ($results as $item) : ?>
                         <div class="col">
                             <div class="card bg-light" style="height:25em">
@@ -145,7 +145,7 @@ try {
 
                                 </div>
                                 <div class="card-footer">
-                                    Cost: <?php se($item, "unit_price"); ?>
+                                 \
 
                                 </div>
                             </div>
