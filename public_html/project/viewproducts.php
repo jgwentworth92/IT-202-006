b55ed47036b9e7b49b89b540b3caeae83dbfe405
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-18 15:33:45 -0500

    pic update

--- public_html/project/viewproducts.php
+++ public_html/project/viewproducts.php
@@ -187,7 +187,7 @@ try {
                     <div class="col">
                         <div class="card  d-flex flex-column justify-content-center   bg-light" style="height:30em">
                             <?php if (se($item, "image", "", false)) : ?>
-                                <img src="<?php se($item, "image"); ?>" class="card-img-top"  style= "width:auto;height:100%;" alt="...">
+                                <img src="<?php se($item, "image"); ?>" class="card-img-top"  style= " max-width:20%; max-height:20%;width:auto;height:100%;" alt="...">
                             <?php endif; ?>
                             <div class="card-header">
                                 <a href="<?php echo get_url('item_details.php'); ?>?id=<?php se($item, "id"); ?>">Item Details</a>
