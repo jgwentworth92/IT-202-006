91873b6058da00d8e706f84f2b7b91f59f1b2820
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-21 14:34:03 -0500

    orders sql creation

--- public_html/project/sql/create_orders.sql
+++ public_html/project/sql/create_orders.sql
@@ -3,9 +3,10 @@ CREATE TABLE IF NOT EXISTS Orders(
     user_id int,
     total   DECIMAL(19 , 4 ) NOT NULL,
      money_recieved  DECIMAL(19 , 4 ) NOT NULL,
+       payment_method varchar(30),
     address text,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
   
 )
