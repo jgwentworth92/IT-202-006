40996f4580d2b80701b25a6e2c69fe9dff9e9df2
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-21 23:43:06 -0500

    checkout form

--- public_html/project/order_confirmation.php
+++ public_html/project/order_confirmation.php
@@ -8,10 +8,12 @@ if (isset($_POST["submit"])) {
     $total = se($_POST, "payment_amount", "", false);
     $payment_type= se($_POST, "payment_type", "", false);
     $Address= se($_POST, "address", "", false).se($_POST, "address2", "", false).se($_POST, "country", "", false) .se($_POST, "state", "", false).se($_POST, "state", "", false);
-    
+    error_log(var_export($Address, true));
+    error_log(var_export($total, true));
+    error_log(var_export($payment_type, true));
     $user_id = get_user_id();
     
- 
+    $db = getDB();
 
         $stmt = $db->prepare("INSERT INTO Orders (total, money_recieved, user_id,payment_method,address) VALUES(:total, :recieved, :userID,:pay_type,:destination");
         $stmt->bindValue(":total", $total, PDO::PARAM_STR);
