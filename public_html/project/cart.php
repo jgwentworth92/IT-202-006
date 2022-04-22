a73c166d58ad53652c657c44bc9cd9366653eccc
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-21 21:03:59 -0500

    checkout form

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -113,7 +113,7 @@ try {
 <!-- Modal -->
 <div class="modal fade" id="checkout" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content justify-content-center">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Shipping Form</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
