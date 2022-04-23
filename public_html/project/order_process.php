8a861afd620c2b5e59372ec0863b84197e7a1b64
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-23 01:25:07 -0500

    db fixes

--- public_html/project/order_process.php
+++ public_html/project/order_process.php
@@ -103,7 +103,7 @@ if (!$hasError) {
     if ($next_order_id > 0) {
         $stmt = $db->prepare("UPDATE Products 
         set stock = stock - (select IFNULL(quantity, 0) FROM JG_Cart WHERE item_id = Products.id and user_id = :uid) 
-        WHERE id in (SELECT item_id from RM_Cart where user_id = :uid)");
+        WHERE id in (SELECT item_id from 'JG_Cart where user_id = :uid)");
         try {
             $stmt->execute([":uid" => $user_id]);
         } catch (PDOException $e) {
@@ -115,7 +115,7 @@ if (!$hasError) {
     }
     if ($next_order_id > 0) {
         $stmt = $db->prepare("INSERT INTO Orderitems (item_id, user_id, quantity, cost, order_id) 
-        SELECT item_id, user_id, RM_Cart.quantity, cost, :order_id FROM JG_Cart JOIN Products on JG_Cart.item_id = Products.id
+        SELECT item_id, user_id, JG_Cart.quantity, cost, :order_id FROM JG_Cart JOIN Products on JG_Cart.item_id = Products.id
          WHERE user_id = :uid");
         try {
             $stmt->execute([":uid" => $user_id, ":order_id" => $next_order_id]);
