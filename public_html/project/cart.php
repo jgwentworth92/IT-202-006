17176490ddd2ddcfac8c0ff06c6f9948faf87458
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-24 16:41:17 -0500

    order process complete

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -254,13 +254,7 @@ try {
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#checkout">
                                 Checkout
                             </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card width:20%">
-                        <div class="card-body">
-                            <a href="<?php echo get_url('remove_all.php') ?> " class="btn btn-primary">Delete</a>
+                            <a href="<?php echo get_url('remove_all.php') ?> " class="btn btn-primary">Delete</
                         </div>
                     </div>
                 </div>
