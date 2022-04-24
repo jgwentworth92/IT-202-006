2b33022ea3044ed436982ac99df3fe8f9869fca0
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-24 16:34:42 -0500

    order process complete

--- public_html/project/cart.php
+++ public_html/project/cart.php
@@ -247,7 +247,7 @@ try {
 
             <div class="row">
                 <div class="col-sm-6">
-                    <div class="card mb-3">
+                    <div class="card ">
                         <div class="card-body">
                             <h5 class="card-title">Proceed to checkout</h5>
                             <p class="card-text">Total: $ <?php se($total_cost, null, "N/A"); ?></p>
@@ -257,13 +257,7 @@ try {
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div class="card width:20%">
-                        <div class="card-body">
-                            <a href="<?php echo get_url('remove_all.php') ?> " class="btn btn-primary">Delete all</a>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
             <div class="col">
                 <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4 g-4">
