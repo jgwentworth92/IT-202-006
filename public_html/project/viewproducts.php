bd23fcda00275754f1b4bd5006df659b319e14c3
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-24 17:12:01 -0500

    card changes

--- public_html/project/viewproducts.php
+++ public_html/project/viewproducts.php
@@ -25,7 +25,7 @@ $hasError = false;
 if (isset($_POST["submit"])) {
     $item_id = (int)se($_POST, "item_id", null, false);
     $amount = (int)se($_POST, "amount", "", false);
-    $cost=se($_POST, "cost","",false);
+    $cost = se($_POST, "cost", "", false);
     // makes sures entered quantity is not negative 
     if ($amount <= 0) {
         $hasError = true;
@@ -42,7 +42,7 @@ if (isset($_POST["submit"])) {
         $stmt->bindValue(":quantity", $amount, PDO::PARAM_INT);
         $stmt->bindValue(":userID", get_user_id(), PDO::PARAM_INT);
         $stmt->bindValue(":unit_cost", $cost, PDO::PARAM_STR);
-     
+
         try {
             $stmt->execute();
             flash("Successfully added to cart!", "success");
@@ -178,60 +178,57 @@ try {
 
     <div class="container-fluid">
         <h1>Shop</h1>
-        
+
         <div class="row">
             <div class="col">
                 <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
                     <?php foreach ($results as $item) : ?>
                         <div class="col">
-                            <div class="card  d-flex flex-column justify-content-center   bg-light" style="height:35em">
-                                <?php if (se($item, "image", "", false)) : ?>
-                                    <img src="<?php se($item, "image"); ?>" class="card-img-top mx-auto" style=" max-width:20%; max-height:30%;width:auto;height:100%;" alt="...">
-                                <?php endif; ?>
-                                <div class="card-header">
-                                    <a href="<?php echo get_url('item_details.php'); ?>?id=<?php se($item, "id"); ?>">Item Details</a>
-                            
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Name: <?php se($item, "name"); ?></h5>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Description:
-                                            <?php
-                                            // truncates description 
-                                            $STR = strval(se($item, "description", "", false));
-                                            if (strlen($STR) > 100) {
-                                                $shortdesc = truncateWords($STR, 2, "...");
-                                                se($shortdesc);
-                                            } else {
-                                                se($item, "description");
-                                            }
-                                            ?></li>
-                                        <li class="list-group-item">Category: <?php se($item, "category"); ?></li>
-                                        <li class="list-group-item">Stock: <?php se($item, "stock"); ?></li>
-                                        <li class="list-group-item"> Cost: <?php se($item, "unit_price"); ?></li>
-                                    </ul>
-
-                                    <div class="card-body">
-                                        <form name="submit" method="POST" onsubmit="return validate(this);">
-                                            <div class="col-auto">
-                                                <label class="visually-hidden" for="amount">quantity</label>
-                                                <input class="form-control" type="number" step="1" name="amount" required />
-                                                <input class="form-control" type="hidden" name="item_id" value="<?php se($item, "id"); ?>" />
-                                                <input class="form-control" type="hidden" name="cost" value="<?php se($item, "unit_price"); ?>" />
-                                            </div>
-                                            <div class="col-auto">
-                                                <input class="btn btn-primary" type="submit" value="add to cart" name="submit" />
-                                            </div>
-
-
-                                        </form>
-
-
+                            <div class="card text-center   mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <?php if (se($item, "image", "", false)) : ?>
+                                            <img src="<?php se($item, "image"); ?>" class="card-img-top mx-auto" style=" max-width:20%; max-height:30%;width:auto;height:100%;" alt="...">
+                                        <?php endif; ?>
+                                        <div class="card-header">
+                                            <a href="<?php echo get_url('item_details.php'); ?>?id=<?php se($item, "id"); ?>">Item Details</a>
+                                            <?php if (has_role("Admin")) : ?>
+                                                <a href="<?php echo get_url('admin/edit_item.php'); ?>?id=<?php se($item, "id"); ?>">Edit</a>
+                                            <?php endif; ?>
+                                        </div>
                                     </div>
-
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Name: <?php se($item, "name"); ?></h5>
+                                            <p class="card-text">Description: <?php
+                                                                                // truncates description 
+                                                                                $STR = strval(se($item, "description", "", false));
+                                                                                if (strlen($STR) > 100) {
+                                                                                    $shortdesc = truncateWords($STR, 2, "...");
+                                                                                    se($shortdesc);
+                                                                                } else {
+                                                                                    se($item, "description");
+                                                                                }
+                                                                                ?> </p>
+                                            <p class="card-text">Category: <?php se($item, "category"); ?></p>
+                                            <p class="card-text">Stock: <?php se($item, "stock"); ?></p>
+                                            <p class="card-text"> Cost: <?php se($item, "unit_price"); ?></p>
+                                            <form name="submit" method="POST" onsubmit="return validate(this);">
+                                                <div class="col-auto">
+                                                    <label class="visually-hidden" for="amount">quantity</label>
+                                                    <input class="form-control" type="number" step="1" name="amount" required />
+                                                    <input class="form-control" type="hidden" name="item_id" value="<?php se($item, "id"); ?>" />
+                                                    <input class="form-control" type="hidden" name="cost" value="<?php se($item, "unit_price"); ?>" />
+                                                </div>
+                                                <div class="col-auto">
+                                                    <input class="btn btn-primary" type="submit" value="add to cart" name="submit" />
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
-
                             </div>
+
                         </div>
                     <?php endforeach; ?>
                 <?php endif; ?>
