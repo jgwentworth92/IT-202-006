5d5a0e72a5e73543da799e017c079a0b3988f22b
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-24 17:32:01 -0500

    card changes

--- public_html/project/viewproducts.php
+++ public_html/project/viewproducts.php
@@ -184,7 +184,7 @@ try {
                 <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
                     <?php foreach ($results as $item) : ?>
                         <div class="col">
-                            <div class="card text-center  justify-content-center  mb-3" style="height:30em; max-width: 30rem;">
+                            <div class="card text-center  justify-content-center  mb-3" style="height:25em; max-width: 30rem;">
                                 <div class="row g-0">
                                     <div class="col-md-4">
                                         <?php if (se($item, "image", "", false)) : ?>
