b67c67493090f6bc6eba1e134bb55179a8da52c2
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-25 16:32:16 -0500

    test

--- public_html/project/orderdetails.php
+++ public_html/project/orderdetails.php
@@ -39,7 +39,7 @@ require_once(__DIR__ . "/../../partials/flash.php");
 
     <div class="container-fluid">
         <div class="col">
-            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4 g-4">
+            <div class=" d-inline row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4 g-4">
                 <?php foreach ($results as $item) : ?>
                     <div class="col">
                         <div class="card text-white bg-dark text-center justify-content-center   bg-light" style="height:10em; max-width: 18rem;">
