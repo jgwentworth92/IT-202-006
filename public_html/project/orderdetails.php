7ad305239635ce0cf46fb1e91bf05d87af3cf826
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-26 14:41:26 -0500

    table test

--- public_html/project/orderdetails.php
+++ public_html/project/orderdetails.php
@@ -35,58 +35,39 @@ require_once(__DIR__ . "/../../partials/flash.php");
     <h1>
         Order ID <?php se($order_id, null, "N/A"); ?>
         Payment type <?php se($payment_method, null, "N/A"); ?>
+
+        Total $<?php se($total_cost, null, "N/A"); ?>
     </h1>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
-                    <?php foreach ($results as $item) : ?>
-                        <div class="col">
-                            <div class="card text-white bg-dark text-center justify-content-center   bg-light" style="height:10em; max-width: 18rem;">
-                                <div class="card-header">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Item SKU: <?php se($item, "item_id"); ?></h5>
-                                    <p class="card-text">Price: <?php se($item, "cost"); ?></p>
-                                    <p class="card-text">Amount: <?php se($item, "quantity"); ?></p>
-                                    <p class="card-text">Subtotal: <?php se($item, "subtotal"); ?></p>
-
-                                </div>
-
-                            </div>
-
-                        <?php endforeach; ?>
-                        </div>
-                </div>
-            </div>
-            <div class="col-4" style="min-width:10em">
-
-
-
-                <table class="table">
-                    <?php foreach ($results as $item) : ?>
-
-                        <thead>
-                            <th scope="col">Item SKU</th>
-                            <th scope="col"> per unit cost</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">subtotal</th>
-
-                            <th>Actions</th>
-                        </thead>
-
-                        <tr>
-                            <td>Item SKU: <?php se($item, "item_id"); ?></td>
-                            <td>Item SKU: <?php se($item, "cost"); ?></td>
-                            <td>Item SKU: <?php se($item, "quantity"); ?></td>
-                            <td>Item SKU: <?php se($item, "subtotal"); ?></td>
 
 
 
 
-                            <td>
-                                <a href="edit_item.php?id=<?php se($record, "id"); ?>">Edit</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
+    <table class="table">
+
+
+        <thead>
+            <th scope="col">Item SKU</th>
+            <th scope="col"> per unit cost</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">subtotal</th>
+
+            <th>Actions</th>
+        </thead>
+        <?php foreach ($results as $item) : ?>
+
+            <tr>
+                <td> <?php se($item, "item_id"); ?></td>
+                <td><?php se($item, "cost"); ?></td>
+                <td><?php se($item, "quantity"); ?></td>
+                <td> <?php se($item, "subtotal"); ?></td>
+
+
+
+
+                <td>
+                    <a href="edit_item.php?id=<?php se($item, "item_id"); ?>">Edit</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+</div>
