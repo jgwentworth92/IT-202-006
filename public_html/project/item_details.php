ae8385229e083c7017a948eb95d54e068e40f55c
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-05-01 16:14:11 -0500

    rating  table

--- public_html/project/item_details.php
+++ public_html/project/item_details.php
@@ -189,7 +189,7 @@ try {
         <div class="col">
             <div class="card bg-light ">
                 <div class="card-header">
-                  Average  USer Rating: <?php se($item, "avg_rating"); ?> /5 ☆ 
+                  Average  User Rating: ☆ <?php se($item, "avg_rating"); ?> /5 
                 </div>
                 <?php if (se($item, "image", "", false)) : ?>
                     <img src="<?php se($item, "image"); ?>" class="card-img-top" style="max-width:20%;" alt="...">
