369ac89299924da1ddb10c338dc12f0e8dcf4f19
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-30 23:02:03 -0500

    rating  table

--- public_html/project/item_details.php
+++ public_html/project/item_details.php
@@ -8,8 +8,8 @@ $db = getDB();
 
 
 $results = [];
-$boughtCHK=false;
-$results2=[];
+$boughtCHK = false;
+$results2 = [];
 $item_id = se($_GET, "id", -1, false);
 // makes sures entered quantity is not negative 
 
@@ -27,33 +27,36 @@ try {
 }
 
 if (is_logged_in()) {
-    $user=get_user_id();
+    $user = get_user_id();
     $params[":uid"] = "$user";
     $params[":item"] = "$item_id";
 
-$stmt = $db->prepare("SELECT id from OrderItems WHERE item_id = :item and user_id=:uid");
-foreach ($params as $key => $value) {
-    error_log(var_export($value, true));
-    $type = is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
-    $stmt->bindValue($key, $value, $type);
-}
-$params = null;
-try {
-    $stmt->execute($params);
-    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if ($r) {
-        $results2 = $r;
+    $stmt = $db->prepare("SELECT id from OrderItems WHERE item_id = :item and user_id=:uid");
+    foreach ($params as $key => $value) {
+        error_log(var_export($value, true));
+        $type = is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
+        $stmt->bindValue($key, $value, $type);
     }
+    $params = null;
+    try {
+        $stmt->execute($params);
+        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($r) {
+            $results2 = $r;
+        }
 
-    if (count($results2) > 0)
-    {$boughtCHK=true;}
-    $rating = (int)se($_POST, "rating", "", false);
-    error_log(var_export($rating." rating value" , true));
-
-} catch (PDOException $e) {
-    error_log(var_export($e, true));
-    flash("Error fetching records we in it bby", "danger");
-}}
+        if (count($results2) > 0) {
+            $boughtCHK = true;
+        }
+        $rating = (int)se($_POST, "rating", "", false);
+        error_log(var_export($rating . " rating value", true));
+        $review = se($_POST, "comment", "", false);
+        error_log(var_export($review. " review value", true));
+    } catch (PDOException $e) {
+        error_log(var_export($e, true));
+        flash("Error fetching records we in it bby", "danger");
+    }
+}
 
 
 
@@ -61,7 +64,7 @@ try {
 if (isset($_POST["add"])) {
     $item_id = (int)se($_POST, "item_id", null, false);
     $amount = (int)se($_POST, "amount", "", false);
-    $cost=se($_POST, "cost",null,false);
+    $cost = se($_POST, "cost", null, false);
     $hasError = false;
     // makes sures entered quantity is not negative 
     if ($amount <= 0) {
@@ -94,75 +97,75 @@ if (isset($_POST["add"])) {
 
 
 <div class="container-fluid">
-<?php foreach ($results as $item) : ?>
-    <div class="col">
-        <div class="card bg-light ">
-            <div class="card-header">
-                Item Details
-            </div>
-            <?php if (se($item, "image", "", false)) : ?>
-                <img src="<?php se($item, "image"); ?>" class="card-img-top" style="max-width:20%;" alt="...">
-            <?php endif; ?>
-            <div class="card-body">
-                <h5 class="card-title">Name: <?php se($item, "name"); ?></h5>
-                <p class="card-text">Description: <?php se($item, "description"); ?></p>
-                <p class="card-text">Category: <?php se($item, "category"); ?></p>
-                <p class="card-text">Stock: <?php se($item, "stock"); ?></p>
-            </div>
-            <div class="card-footer">
-                Cost: <?php se($item, "unit_price"); ?>
-
-                <?php if (is_logged_in()) : ?>
-                    <form name="submit" method="POST" onsubmit="return validate(this);">
-                        <label class="form-label" for="amount">Quantity</label>
-                        <input class="form-control" type="number" step="1" name="amount" required />
-                        <input class="form-control" type="hidden" name="item_id" value="<?php se($item, "id"); ?>" />
-                        <input class="form-control" type="hidden" name="cost" value="<?php se($item, "unit_price"); ?>" />
-                        <input class="form-control" type="hidden" name="avail_amount" value="<?php se($item, "stock"); ?>" />
-                        <input class="btn btn-primary" type="submit" value="add to cart" name="add" />
-                    </form>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#form"> Leave a review </button>
+    <?php foreach ($results as $item) : ?>
+        <div class="col">
+            <div class="card bg-light ">
+                <div class="card-header">
+                    Item Details
+                </div>
+                <?php if (se($item, "image", "", false)) : ?>
+                    <img src="<?php se($item, "image"); ?>" class="card-img-top" style="max-width:20%;" alt="...">
                 <?php endif; ?>
-            </div>
-        </div>
-        <?php if (has_role("Admin")) : ?>
-
-            <td>
-                <a href="<?php echo get_url('admin/edit_item.php'); ?>?id=<?php se($item, "id"); ?>">Edit</a>
-            </td>
-        <?php endif; ?>
-    </div>
-   
-<?php endforeach; ?>
-<div class="col-4" style="min-width:30em">
-
-
-
-<div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-        <form class="text-center d-block"  method="POST">
-            <div class="text-right cross"> <i class="fa fa-times mr-2"></i> </div>
-            <div class="card-body text-center"> <img src=" https://i.imgur.com/d2dKtI7.png" height="100" width="100">
-                <div class="comment-box text-center">
-                    <h4>Add a comment</h4>
-                    <div class="rating"> 
-                        <input type="radio" name="rating" value="5" id="5">
-                        <label for="5">☆</label>
-                         <input type="radio" name="rating" value="4" id="4">
-                         <label for="4">☆</label>
-                          <input type="radio" name="rating" value="3" id="3">
-                         <label for="3">☆</label> 
-                         <input type="radio" name="rating" value="2" id="2">
-                         <label for="2">☆</label>
-                          <input type="radio" name="rating" value="1" id="1">
-                         <label for="1">☆</label> 
+                <div class="card-body">
+                    <h5 class="card-title">Name: <?php se($item, "name"); ?></h5>
+                    <p class="card-text">Description: <?php se($item, "description"); ?></p>
+                    <p class="card-text">Category: <?php se($item, "category"); ?></p>
+                    <p class="card-text">Stock: <?php se($item, "stock"); ?></p>
                 </div>
-                    <div class="comment-area"> <textarea class="form-control" placeholder="what is your view?" rows="4"></textarea> </div>
-                    <div class="text-center mt-4"> <button class="btn btn-success send px-5">Send message <i class="fa fa-long-arrow-right ml-1"></i></button> </div>
+                <div class="card-footer">
+                    Cost: <?php se($item, "unit_price"); ?>
+
+                    <?php if (is_logged_in()) : ?>
+                        <form name="submit" method="POST" onsubmit="return validate(this);">
+                            <label class="form-label" for="amount">Quantity</label>
+                            <input class="form-control" type="number" step="1" name="amount" required />
+                            <input class="form-control" type="hidden" name="item_id" value="<?php se($item, "id"); ?>" />
+                            <input class="form-control" type="hidden" name="cost" value="<?php se($item, "unit_price"); ?>" />
+                            <input class="form-control" type="hidden" name="avail_amount" value="<?php se($item, "stock"); ?>" />
+                            <input class="btn btn-primary" type="submit" value="add to cart" name="add" />
+                        </form>
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#form"> Leave a review </button>
+                    <?php endif; ?>
                 </div>
             </div>
+            <?php if (has_role("Admin")) : ?>
+
+                <td>
+                    <a href="<?php echo get_url('admin/edit_item.php'); ?>?id=<?php se($item, "id"); ?>">Edit</a>
+                </td>
+            <?php endif; ?>
         </div>
-        <form>
-    </div>
-</div>
+
+    <?php endforeach; ?>
+    <div class="col-4" style="min-width:30em">
+
+
+
+        <div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <form class="text-center d-block" method="POST">
+                        <div class="text-right cross"> <i class="fa fa-times mr-2"></i> </div>
+                        <div class="card-body text-center"> <img src=" https://i.imgur.com/d2dKtI7.png" height="100" width="100">
+                            <div class="comment-box text-center">
+                                <h4>Add a comment</h4>
+                                <div class="rating">
+                                    <input type="radio" name="rating" value="1" id="1">
+                                    <label for="1">1 ☆</label>
+                                    <input type="radio" name="rating" value="2" id="2">
+                                    <label for="2">2 ☆</label>
+                                    <input type="radio" name="rating" value="3" id="3">
+                                    <label for="3">3 ☆</label>
+                                    <input type="radio" name="rating" value="4" id="4">
+                                    <label for="4">4 ☆</label>
+                                    <input type="radio" name="rating" value="5" id="5">
+                                    <label for="5">5 ☆</label>
+                                </div>
+                                <div class="comment-area"> <textarea name="comment" class="form-control" placeholder="what is your view?" rows="4"></textarea> </div>
+                                <div class="text-center mt-4"> <button  type="submit" value="add a review" name="review" class="btn btn-success send px-5">Submit review <i class="fa fa-long-arrow-right ml-1"></i></button> </div>
+                            </div>
+                        </div>
+                </div>
+                <form>
+            </div>
+        </div>
