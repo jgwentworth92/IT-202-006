014b87904e8fd102792efb34f5738d1a6eb86eaf
jon <95505687+jgwentworth92@users.noreply.github.com>
2022-04-30 21:01:34 -0500

    rating  table

--- public_html/project/item_details.php
+++ public_html/project/item_details.php
@@ -101,5 +101,24 @@ if (isset($_POST["add"])) {
             </td>
         <?php endif; ?>
     </div>
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#form"> See Modal with Form </button>
 <?php endforeach; ?>
 <div class="col-4" style="min-width:30em">
+
+
+
+<div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="text-right cross"> <i class="fa fa-times mr-2"></i> </div>
+            <div class="card-body text-center"> <img src=" https://i.imgur.com/d2dKtI7.png" height="100" width="100">
+                <div class="comment-box text-center">
+                    <h4>Add a comment</h4>
+                    <div class="rating"> <input type="radio" name="rating" value="5" id="5"><label for="5">☆</label> <input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> <input type="radio" name="rating" value="3" id="3"><label for="3">☆</label> <input type="radio" name="rating" value="2" id="2"><label for="2">☆</label> <input type="radio" name="rating" value="1" id="1"><label for="1">☆</label> </div>
+                    <div class="comment-area"> <textarea class="form-control" placeholder="what is your view?" rows="4"></textarea> </div>
+                    <div class="text-center mt-4"> <button class="btn btn-success send px-5">Send message <i class="fa fa-long-arrow-right ml-1"></i></button> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
